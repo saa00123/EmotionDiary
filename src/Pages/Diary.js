793ce54e63b2654d59e2e251038f59c8e9dc31a0
Choejeonghun <saa00123@naver.com>
2023-07-05 23:41:15 +0900

    feat: useParams, QueryString, useNavigate 활용

--- src/Pages/Diary.js
+++ src/Pages/Diary.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Diary = () => {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <h1>이곳은 일기 상세 페이지입니다.</h1>
--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <div>
       <h1>이곳은 수정 입니다.</h1>
+      <button onClick={() => setSearchParams({ who: "test" })}>QS바꾸기</button>
+      <button onClick={() => navigate("/")}>HOME으로 가기</button>
     </div>
   );
 };
