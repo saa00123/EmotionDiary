114b9e1f37acb8cffd561d81f10aac619eeee0c0
Choejeonghun <saa00123@naver.com>
2023-07-08 21:14:34 +0900

    feat: MyButton MyHeader components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+// pages
 import Home from "./Pages/Home";
 import New from "./Pages/New";
 import Edit from "./Pages/Edit";
 import Diary from "./Pages/Diary";
 
-import RouteTest from "./Components/RouteTest";
+//components
+import MyButton from "./Components/MyButton";
+import MyHeader from "./Components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
+        <MyHeader headText='App' />
+        <MyButton
+          text={"button"}
+          onClick={() => alert("button click")}
+          type={"positive"}
+        />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
           <Route path='/edit' element={<Edit />} />
           <Route path='/diary/:id' element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
---
+++ src/Components/MyHeader.js
@@ -0,0 +1,11 @@
+const MyHedaer = ({ headText, leftChild, rightChild }) => {
+  return (
+    <header>
+      <div className='head_btn_left'>{leftChild}</div>
+      <div className='head_text'>{headText}</div>
+      <div className='head_btn_right'>{rightChild}</div>
+    </header>
+  );
+};
+
+export default MyHedaer;
